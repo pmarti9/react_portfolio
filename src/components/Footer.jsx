679469dd07e2d2726086ffd87f780ca161ec6d4b
pmarti99 <parkerjmartin883@gmail.com>
2020-07-30 20:29:55 -0600

    working navabar and footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Footer() {
+    return (
+    <footer>
+         <br />
+         Parker Martin
+         <br />
+         <br />
+         Click on my email address to send me an email! <a href="mailto:parkerjmartin883@gmail.com">parkerjmartin883@gmail.com</a>
+         <br /><br />
+      </footer>
+    )
+
+}
+
+export default Footer;
