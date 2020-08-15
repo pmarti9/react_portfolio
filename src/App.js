32a2d3fbdbed17813ce6020273ec1a1871b71ce7
pmarti99 <parkerjmartin883@gmail.com>
2020-08-15 16:06:55 -0600

    adding browser router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch} from "react-router-dom"
+import { BrowserRouter as Router, Route, Switch, BrowserRouter} from "react-router-dom"
 // import logo from './logo.svg';
+
+
+// \\
 import './App.css';
 import Home from "./components/Home/Home"
 import Navbar from "./components/Navbar/Navbar"
@@ -12,10 +15,11 @@ import Portfolio from './components/Portfolio/Portfolio';
 function App() {
   return ( 
     <Router>
+    <BrowserRouter basename={process.env.PUBLIC_URL}></BrowserRouter>
     <div>
     <Navbar />
       <Switch>
-        <Route exact path={["/", "/Home","index.html","/react_portfolio"]}>
+        <Route exact path={["/", "/Home"]}>
           <Home />
         </Route>
         <Route exact path={['/Contact']}>
